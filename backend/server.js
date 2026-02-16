5b956a5e93d6cc9beae1a366a10e9ff78791c56d
P666R <sam469roger@gmail.com>
2026-02-16 06:42:10 +0530

    feat(backend): update server to include routes logging and error handling

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 import 'dotenv/config';
-import morgan from 'morgan';
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import mongoSanitize from 'express-mongo-sanitize';
-import { connectionToDB } from './config/connect-db.js';
-import { morganMiddleware, systemLogs } from './utils/logger.js';
+import {
+  errorHandlerMiddleware,
+  notFoundHandlerMiddleware,
+} from '#middlewares/error-middleware.js';
+import { createChild } from '#utils/logger.js';
+import { envConfig } from '#config/env-config.js';
+import { authRouter } from '#routes/auth-routes.js';
+import { connectionToDB } from '#config/connect-db.js';
+import { httpLoggingMiddleware } from '#middlewares/logging-middleware.js';
 
-const PORT = process.env.PORT || 1997;
-const NODE_ENV = process.env.NODE_ENV || 'production';
+const { PORT, NODE_ENV } = envConfig;
+
+// Logger instance for server logs
+const logger = createChild({ service: 'server' });
 
 // Connect to database
 await connectionToDB();
@@ -15,25 +22,28 @@ await connectionToDB();
 // Initialize express app
 const app = express();
 
-// Logger instance for server logs
-const logger = systemLogs.child({ service: 'server' });
+// Trust proxy sent by nginx
+// 1 represents the single hop from Nginx container to API container
+app.set('trust proxy', 1);
 
-NODE_ENV === 'development' && app.use(morgan('dev'));
-
+// Middlewares
+app.use(httpLoggingMiddleware());
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: false }));
-
 app.use(cookieParser());
 
-app.use(mongoSanitize());
-
-app.use(morganMiddleware);
-
+// Routes
 app.get('/api/v1/test', (_req, res) => {
-  res.status(200).json({ message: 'API is working properly' });
+  res.status(200).json({ message: 'Welcome to the Billify API' });
 });
 
+app.use('/api/v1/auth', authRouter);
+
+// Error handlers
+app.use(notFoundHandlerMiddleware);
+app.use(errorHandlerMiddleware);
+
+// Start server
 app.listen(PORT, () => {
   logger.info(`server: running in ${NODE_ENV} mode on port ${PORT}`);
 });
