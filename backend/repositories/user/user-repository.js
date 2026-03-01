d57ebc1317bff8f777144a9935562ddd30623802
P666R <sam469roger@gmail.com>
2026-03-01 23:06:12 +0530

    feat(backend): implement user repository

--- backend/repositories/user/user-repository.js
+++ backend/repositories/user/user-repository.js
@@ -1,12 +1,22 @@
 import { User } from '#models/user-model.js';
 import { VerifyResetToken } from '#models/verify-reset-token-model.js';
 
+// User operations
 export const findUserByEmail = (data) => User.findOne(data);
 
-export const findUserbyId = (data) => User.findById(data);
+export const findUserByEmailWithPassword = (data) =>
+  User.findOne(data).select('+password');
+
+export const findUserById = (id) => User.findById(id);
+
+export const findUserByRefreshToken = (data) => User.findOne(data);
 
 export const createUser = (data) => User.create(data);
 
+// VerifyResetToken operations
 export const createVerifyResetToken = (data) => VerifyResetToken.create(data);
 
 export const findVerifyResetToken = (data) => VerifyResetToken.findOne(data);
+
+export const deleteVerifyResetToken = (data) =>
+  VerifyResetToken.findOneAndDelete(data);
