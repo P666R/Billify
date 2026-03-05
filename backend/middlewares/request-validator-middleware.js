b57010712500efbeb9da70d54550539eaa9e7b4e
P666R <sam469roger@gmail.com>
2026-03-05 12:47:22 +0530

    fix(backend): fix to modify object content rather than reassign the property

--- backend/middlewares/request-validator-middleware.js
+++ backend/middlewares/request-validator-middleware.js
@@ -1,11 +1,12 @@
-// usage router.post('/...', validateRequest({ body: registerUserSchema }), controller);
 export const validateRequest = (schemas) => (req, _res, next) => {
   try {
     const targets = ['body', 'params', 'query', 'cookies'];
 
     for (const target of targets) {
-      if (schemas[target]) {
-        req[target] = schemas[target].parse(req[target]);
+      if (schemas[target] && req[target]) {
+        const validatedData = schemas[target].parse(req[target]);
+        // Modify the object content instead of reassigning the property
+        Object.assign(req[target], validatedData);
       }
     }
     next();
