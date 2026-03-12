0ca41276fa1ebe75885e07f830a0f9f966c11293
P666R <sam469roger@gmail.com>
2026-03-12 19:50:17 +0530

    feat(frontend): implement main and app components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,38 @@
+import { Routes, Route } from 'react-router';
+import { Box, CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import { ToastContainer } from 'react-toastify';
+
+import { Footer } from './components/Footer';
+import { Layout } from './components/Layout';
+import { NotFound } from './components/NotFound';
+
+import { HomePage } from './pages/HomePage';
+
+import { useTitle } from './hooks/useTitle';
+
+import { customTheme } from './customTheme';
+
 const App = () => {
-  return <div>App!!</div>;
+  useTitle('Billify - Home');
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      <Box
+        sx={{ display: 'flex', flexDirection: 'column', minHeight: '100dvh' }}
+      >
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+        <Footer />
+      </Box>
+      <ToastContainer theme="dark" />
+    </ThemeProvider>
+  );
 };
 
 export default App;
