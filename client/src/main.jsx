0fbe0d4e17bc7032aff140b0164dae6084c86421
P666R <sam469roger@gmail.com>
2026-03-24 22:05:57 +0530

    feat(frontend): add redux store to app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,19 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { StrictMode } from 'react';
+import { Provider } from 'react-redux';
+import { store } from './app/store.js';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
