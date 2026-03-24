fbc3a12d9a55959fe8a0e576e2d1d31b692bcd83
P666R <sam469roger@gmail.com>
2026-03-24 21:51:54 +0530

    feat(frontend): implement auth slice

---
+++ client/src/features/auth/authSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const user = JSON.parse(localStorage.getItem('user'));
+
+const initialState = {
+  user: user || null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    logIn: (state, action) => {
+      state.user = action.payload;
+      localStorage.setItem('user', JSON.stringify(action.payload));
+    },
+    logOut: (state) => {
+      state.user = null;
+      localStorage.removeItem('user');
+    },
+  },
+});
+
+export const { logIn, logOut } = authSlice.actions;
+export const authReducer = authSlice.reducer;
