2563f309ad349823034b68b86c89d7f9c21158c4
P666R <sam469roger@gmail.com>
2026-04-01 09:32:42 +0530

    fix(frontend): fix layout issue

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,17 +1,20 @@
 import { Box, Link, Typography } from '@mui/material';
 import { FaRegCopyright } from 'react-icons/fa';
+import { fluidType } from '../customTheme';
 
 const Copyright = () => {
   return (
     <Typography
-      variant="body2"
-      align="center"
+      textAlign="center"
+      variant="h6"
       sx={{
-        color: '#ffffff',
+        color: '#fff',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
         gap: '0.5rem',
+        fontSize: fluidType(12, 14),
+        fontWeight: '500',
       }}
     >
       <FaRegCopyright />
@@ -22,11 +25,22 @@ const Copyright = () => {
 
 export const Footer = () => {
   return (
-    <Box component="footer" sx={{ bgcolor: '#000000', py: '0.8rem' }}>
+    <Box
+      component="footer"
+      elevation={0}
+      sx={{
+        backgroundColor: '#000',
+        backdropFilter: 'blur(16px)',
+        borderTop: '1px solid rgba(255,255,255,0.1)',
+        boxShadow: '0 -4px 20px -4px rgba(0, 0, 0, 0.3)',
+        py: '0.4rem',
+        minHeight: '6dvh',
+      }}
+    >
       <Typography
-        variant="body2"
-        align="center"
-        sx={{ color: '#07f011', mb: '0.2rem' }}
+        textAlign="center"
+        variant="h5"
+        sx={{ color: 'green.main', fontSize: fluidType(12, 14), mb: '0.1rem' }}
       >
         <Link href="https://www.billify.com" color="inherit" underline="hover">
           INVOICING SIMPLIFIED FOR EVERYONE
