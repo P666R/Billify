a10454bc7ff3ea5b9f0291be9185cc8b1d3ca010
P666R <sam469roger@gmail.com>
2026-04-01 09:46:27 +0530

    feat(frontend): add login and navbar page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux';
 import { Routes, Route } from 'react-router';
 import { Box, CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
@@ -8,6 +9,8 @@ import { Layout } from './components/Layout';
 import { NotFound } from './components/NotFound';
 
 import { HomePage } from './pages/HomePage';
+import { Navbar } from './components/Navbar';
+import { LoginPage } from './features/auth/pages/LoginPage';
 import { RegisterPage } from './features/auth/pages/RegisterPage';
 import { VerifiedPage } from './features/auth/pages/VerifiedPage';
 
@@ -17,17 +20,25 @@ import { customTheme } from './customTheme';
 
 const App = () => {
   useTitle('Billify - Home');
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <ThemeProvider theme={customTheme}>
       <CssBaseline />
       <Box
-        sx={{ display: 'flex', flexDirection: 'column', minHeight: '100dvh' }}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          minHeight: '100dvh',
+        }}
       >
+        {user && <Navbar />}
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
             <Route path="register" element={<RegisterPage />} />
             <Route path="auth/verify" element={<VerifiedPage />} />
+            <Route path="login" element={<LoginPage />} />
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
